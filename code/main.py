45c7aba82e795812b82d18f1bdf043353b4b2719
rootamin <osalimohammadamin@gmail.com>
2022-09-29 19:42:50 +0330

    Made the sky

---
+++ code/main.py
@@ -0,0 +1,132 @@
+import pygame, sys
+from settings import *
+from pytmx.util_pygame import load_pygame
+from tile import Tile, CollisionTile, MovingPlatform
+from player import Player
+from pygame.math import Vector2 as vector
+from bullet import Bullet, FireAnimation
+
+class AllSprites(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.offset = vector() # 0 and 0
+
+        # import
+        self.fg_sky = pygame.image.load('../graphics/sky/fg_sky.png').convert_alpha()
+        self.bg_sky = pygame.image.load('../graphics/sky/bg_sky.png').convert_alpha()
+        tmx_map = load_pygame('../data/map.tmx')
+
+        # dimensions
+        self.sky_width = self.bg_sky.get_width() # bg and fg have the same width
+        self.padding = WINDOW_WIDTH / 2 # 136 3:20
+        map_width = tmx_map.tilewidth * tmx_map.width + (2 * self.padding)
+        self.sky_num = int(map_width // self.sky_width)
+
+    def custom_draw(self, player):
+        self.offset.x = player.rect.centerx - WINDOW_WIDTH / 2
+        self.offset.y = player.rect.centery - WINDOW_HEIGHT / 2
+
+        for x in range(self.sky_num):
+            x_pos = -self.padding + (x * self.sky_width) # makes the map blit at the padding
+            self.display_surface.blit(self.bg_sky, (x_pos - self.offset.x / 2.5, 800 - self.offset.y / 2.5))
+            self.display_surface.blit(self.fg_sky, (x_pos - self.offset.x / 2, 800 - self.offset.y / 2))
+
+        for sprite in sorted(self.sprites(), key=lambda sprite: sprite.z):
+            offset_rect = sprite.image.get_rect(center=sprite.rect.center)
+            offset_rect.center -= self.offset
+            self.display_surface.blit(sprite.image, offset_rect)
+
+class Main:
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('Contra')
+        self.clock = pygame.time.Clock()
+
+        # groups
+        self.all_sprites = AllSprites()
+        self.collision_sprites = pygame.sprite.Group()
+        self.platform_sprites = pygame.sprite.Group()
+        self.bullet_sprites = pygame.sprite.Group()
+
+        self.setup()
+
+        # bullet images, we import these here insted of inside the loop for performance sakes(we dont want to load them on each loop)
+        self.bullet_surf = pygame.image.load('../graphics/bullet.png').convert_alpha()
+        self.fire_surfs = [pygame.image.load('../graphics/fire/0.png').convert_alpha(), pygame.image.load('../graphics/fire/1.png').convert_alpha()]
+
+    def setup(self): # we call it under init
+        tmx_map = load_pygame('../data/map.tmx')
+
+        # collision tiles
+        for x, y, surf in tmx_map.get_layer_by_name('Level').tiles():  # this one needs collision mechanics and we are not passing it under the below for loop
+            CollisionTile((x * 64, y * 64), surf, [self.all_sprites, self.collision_sprites])  # multiply by 64 is because of the eath tile in this project is 64 pixels
+
+        # tiles
+        for layer in ['BG', "BG Detail", "FG Detail Bottom", "FG Detail Top"]: # doesnt need any collision mechanics just overlapping for making it look real
+            for x, y, surf in tmx_map.get_layer_by_name(layer).tiles():
+                Tile((x * 64, y * 64), surf, self.all_sprites, LAYERS[layer])  # last attributes returns to z in the tile.py and because the dictionaries key values are the same it would work.
+
+        # objects
+        for obj in tmx_map.get_layer_by_name('Entities'):
+            if obj.name == 'Player':
+                self.player = Player((obj.x, obj.y), self.all_sprites, '../graphics/player', self.collision_sprites, self.shoot)
+
+        self.platform_border_rects = []
+        for obj in tmx_map.get_layer_by_name('Platforms'):
+            if obj.name == 'Platform':
+                MovingPlatform((obj.x, obj.y), obj.image, [self.all_sprites, self.collision_sprites, self.platform_sprites])
+            else: # border
+                border_rect = pygame.Rect(obj.x, obj.y, obj.width, obj.height)
+                self.platform_border_rects.append(border_rect)
+
+    def platform_collisions(self):
+        for platform in self.platform_sprites.sprites():
+            for border in self.platform_border_rects:
+                if platform.rect.colliderect(border):
+                    if platform.direction.y < 0: # up
+                        platform.rect.top = border.bottom
+                        platform.pos.y = platform.rect.y
+                        platform.direction.y = 1 # platform moves downwards
+                    else: # down
+                        platform.rect.bottom = border.top
+                        platform.pos.y = platform.rect.y
+                        platform.direction.y = -1
+            if platform.rect.colliderect(self.player.rect) and self.player.rect.centery > platform.rect.centery: # this makes the platform go up as soon as it hits the players head( and it would prevent the players head from sticking to bottom of the platform)
+                platform.rect.bottom = self.player.rect.top
+                platform.pos.y = platform.rect.y
+                platform.direction.y = -1
+
+    def shoot(self, pos, direction, entity):
+
+        Bullet(pos, self.bullet_surf, direction, [self.all_sprites, self.bullet_sprites])
+        FireAnimation(entity, self.fire_surfs, direction, self.all_sprites)
+
+    def bullet_collision(self):
+        # obstacles
+        for obstacle in self.collision_sprites.sprites():
+            pygame.sprite.spritecollide(obstacle, self.bullet_sprites, True)
+
+        # entities
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            dt = self.clock.tick() / 1000
+            self.display_surface.fill((249, 131, 103))
+
+            self.platform_collisions()
+            self.all_sprites.update(dt)
+            self.bullet_collision()
+            self.all_sprites.custom_draw(self.player)
+
+            pygame.display.update()
+
+if __name__ == '__main__':
+    main = Main()
+    main.run()
---
+++ code/player.py
@@ -0,0 +1,179 @@
+import pygame
+from settings import *
+from pygame.math import Vector2 as vector
+from os import walk
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, groups, path, collision_sprites, shoot):
+        super().__init__(groups)
+        self.import_assets(path)
+        self.frame_index = 0
+        self.status = 'right'
+
+        self.image = self.animations[self.status][self.frame_index]
+        self.rect = self.image.get_rect(topleft=pos)
+        self.z = LAYERS['Level']
+
+        # float based movement
+        self.direction = vector()
+        self.pos = vector(self.rect.topleft)
+        self.speed = 400
+
+        self.import_assets(path)
+
+        # collision
+        self.old_rect = self.rect.copy()
+        self.collision_sprites = collision_sprites
+
+        # vertical movement
+        self.gravity = 15
+        self.jump_speed = 1200
+        self.on_floor = False
+        self.duck = False
+        self.moving_floor = None
+
+        # interaction
+        self.shoot = shoot # we passed a none called function from the main.py
+
+        # create a bullet timer
+        self.can_shoot = True
+        self.shoot_time = None
+        self.cooldown = 200
+
+    def shoot_timer(self):
+        if not self.can_shoot:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.shoot_time > self.cooldown:
+                self.can_shoot = True
+
+    def get_status(self):
+        # idle
+        if self.direction.x == 0 and self.on_floor:
+            self.status = self.status.split('_')[0] + '_idle'
+        # jump
+        if self.direction.y != 0 and not self.on_floor:  # second if is not necessary because it is gonna be false
+            self.status = self.status.split('_')[0] + '_jump'
+
+        # duck
+        if self.on_floor and self.duck:
+            self.status = self.status.split('_')[0] + '_duck'
+
+    def check_contact(self): # here we create a rect below the player and check if its inside of an obstacle or not for the idle/moving/jumping animations
+        bottom_rect = pygame.Rect(0, 0, self.rect.width, 5)
+        bottom_rect.midtop = self.rect.midbottom
+        for sprite in self.collision_sprites.sprites():
+            if sprite.rect.colliderect(bottom_rect):
+                if self.direction.y > 0:
+                    self.on_floor = True
+                if hasattr(sprite, 'direction'): # only a moving platform has a direction (pos and speed would also work as well)
+                    self.moving_floor = sprite
+
+    def import_assets(self, path):
+        self.animations = {}
+        for index, folder in enumerate(walk(path)):
+            if index == 0:
+                for name in folder[1]:
+                    self.animations[name] = []
+            else:
+                for file_name in sorted(folder[2], key=lambda string: int(string.split('.')[0])):
+                    path = folder[0].replace('\\', "/") + '/' + file_name #3
+                    surf = pygame.image.load(path).convert_alpha() #2
+                    key = folder[0].split('\\')[1] #4
+                    self.animations[key].append(surf) #1
+
+    def animate(self, dt):
+        self.frame_index += 7 * dt
+        if self.frame_index >= len(self.animations[self.status]):
+            self.frame_index = 0
+
+        self.image = self.animations[self.status][int(self.frame_index)]
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+        # horizontal input
+        if keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+            self.status = 'right'
+        elif keys[pygame.K_LEFT]:
+            self.direction.x = -1
+            self.status = 'left'
+        else:
+            self.direction.x = 0
+
+        if keys[pygame.K_UP] and self.on_floor:
+            self.direction.y = -self.jump_speed
+
+        if keys[pygame.K_DOWN]:
+            self.duck = True
+        else:
+            self.duck = False
+
+        if keys[pygame.K_SPACE] and self.can_shoot:
+            direction = vector(1, 0) if self.status.split('_')[0] == 'right' else vector(-1, 0)
+            pos = self.rect.center + direction * 60
+            y_offset = vector(0, -16) if not self.duck else vector(0, 10) # we can duck shoot while on air
+            self.shoot(pos + y_offset, direction ,self)
+
+            self.can_shoot = False
+            self.shoot_time = pygame.time.get_ticks()
+
+    def collision(self, direction):
+        for sprite in self.collision_sprites.sprites():
+            if sprite.rect.colliderect(self.rect):
+
+                if direction == 'horizontal':
+                    # left collision
+                    if self.rect.left <= sprite.rect.right and self.old_rect.left >= sprite.old_rect.right:
+                        self.rect.left = sprite.rect.right
+                    # right collision
+                    if self.rect.right >= sprite.rect.left and self.old_rect.right <= sprite.old_rect.left:
+                        self.rect.right = sprite.rect.left
+                    self.pos.x = self.rect.x
+                else:
+                    # vertical collision
+                    if self.rect.bottom >= sprite.rect.top and self.old_rect.bottom <= sprite.old_rect.top:
+                        self.rect.bottom = sprite.rect.top
+                        self.on_floor = True
+                    if self.rect.top <= sprite.rect.bottom and self.old_rect.top >= sprite.old_rect.bottom:
+                        self.rect.top = sprite.rect.bottom
+                    self.pos.y = self.rect.y
+                    self.direction.y = 0
+
+        if self.on_floor and self.direction.y != 0:  # this switches on floor to false of player is floating
+            self.on_floor = False
+
+    def move(self, dt):
+        if self.duck and self.on_floor:
+            self.direction.x = 0
+
+        # horizontal movement + collision
+        self.pos.x += self.direction.x * self.speed * dt
+        self.rect.x = round(self.pos.x)
+        self.collision('horizontal') # this one is a function, dont get confused
+
+        # vertical movement + collision
+        # gravity
+        self.direction.y += self.gravity
+        self.pos.y += self.direction.y * dt
+
+        # glue the player to the platform                              # if the player is not jumping
+        if self.moving_floor and self.moving_floor.direction.y > 0 and self.direction.y > 0: # if platform is going up its fine but here we are checking if the platform is moving downwards to fix the glitching animation
+            self.direction.y = 0
+            self.rect.bottom = self.moving_floor.rect.top
+            self.pos.y = self.rect.y
+            self.on_floor = True
+
+        self.rect.y = round(self.pos.y)
+        self.collision('vertical')
+        self.moving_floor = None # this gonna make the hasattr in check_contant() gone and we would't stick to the platform while jumping when its moving down
+
+    def update(self, dt):
+        self.old_rect = self.rect.copy() # 129 -> 5:00
+        self.input()
+        self.get_status()
+        self.move(dt)
+        self.check_contact()
+        self.animate(dt)
+
+        # timer
+        self.shoot_timer()
